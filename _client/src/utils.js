af77bf5838d7c658f598a0cef1a183844da0aaf8
Hokori23 <hokori23@qq.com>
2020-10-28 11:18:59 +0800

    fix: tinyfix

--- _client/src/utils.js
+++ _client/src/utils.js
@@ -1,41 +1,4 @@
 /**
- * 函数防抖处理
- * @returns { Function }
- */
-const debounce = (callback, delay) => {
-  return (() => {
-    let timer = null;
-    return (...params) => {
-      if (timer) {
-        clearTimeout(timer);
-      }
-      timer = setTimeout(callback, delay, ...params);
-    };
-  })();
-};
-
-/**
- * 函数节流处理
- * @returns { Function }
- */
-const throttle = (callback, delay) => {
-  console.log(delay);
-  return (() => {
-    let timer = null;
-    return (...params) => {
-      if (!timer) {
-        timer = setTimeout(() => {
-          timer = null;
-          callback(...params);
-        }, delay);
-      } else {
-        console.log('throttle');
-      }
-    };
-  })();
-};
-
-/**
  * 计算两点距离
  */
 const getDistance = (x1, y1, x2, y2) => {
@@ -68,4 +31,4 @@ const dealWithResultCode = (code, message, vm) => {
   }
 };
 
-export { debounce, throttle, getDistance, isDef, isUndef, dealWithResultCode };
+export { getDistance, isDef, isUndef, dealWithResultCode };
